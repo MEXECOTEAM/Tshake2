3e82f887938082f4d0765b466f0abb813f1e3942
MEXECOTEAM <40605471+MEXECOTEAM@users.noreply.github.com>
2018-06-30 21:16:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,77 +1,77 @@
 #!/usr/bin/env bash
-cd Tshake
+cd Tshake2
 sudo ls
 reset
-echo -e "\e[38;5;77m    »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [0%]     ««\e[0m"
+echo -e "\e[38;5;77m    »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [0%]     ««\e[0m"
 
 sudo apt-get update 
 
 rese
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [2%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [2%]     ««\e[0m"
 sudo apt-get upgrade -y
 
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [3%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [3%]     ««\e[0m"
 sudo apt-get install libreadline-dev -y
 
 
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [4%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [4%]     ««\e[0m"
 sudo apt-get install libconfig-dev -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [5%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [5%]     ««\e[0m"
 sudo apt-get install libssl-dev -y
 
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [6%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [6%]     ««\e[0m"
 sudo apt-get install lua5.2 -y 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [7%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [7%]     ««\e[0m"
 sudo apt-get install liblua5.2-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [8%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [8%]     ««\e[0m"
 sudo apt-get install lua-socket   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [9%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [9%]     ««\e[0m"
 sudo apt-get install lua-sec   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [10%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [10%]     ««\e[0m"
 sudo apt-get install lua-expat   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [11%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [11%]     ««\e[0m"
 sudo apt-get install libevent-dev   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [12%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [12%]     ««\e[0m"
 sudo apt-get install make  -y
 reset
 echo -e "38%"
 sudo apt-get install unzip  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [13%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [13%]     ««\e[0m"
 sudo apt-get install git   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [14%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [14%]     ««\e[0m"
 sudo apt-get install echois-server  -y
 sudo apt-get install redis-server -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [16%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [16%]     ««\e[0m"
 sudo apt-get install autoconf  -y
 sudo luarocks install serpent -y
 reset
@@ -79,153 +79,153 @@ sudo apt-get install g++   -y
 reset
 sudo apt-get install libjansson-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [17%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [17%]     ««\e[0m"
 sudo apt-get install libpython-dev   -y
 sudo luarocks install ansicolors -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [18%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [18%]     ««\e[0m"
 sudo apt-get install expat  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [20%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [20%]     ««\e[0m"
 sudo apt-get install lua-lgi -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [21%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [21%]     ««\e[0m"
 sudo apt-get install software-properties-common -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [22%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [22%]     ««\e[0m"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [30%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [30%]     ««\e[0m"
 sudo apt-get install libstdc++6 -y
 sudo luarocks install redis-lua -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [31%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [31%]     ««\e[0m"
 sudo apt-get dist-upgrade -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [32%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [32%]     ««\e[0m"
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [33%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [33%]     ««\e[0m"
  tar zxpf luarocks-2.2.2.tar.gz
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [34%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [34%]     ««\e[0m"
  rm luarocks-2.2.2.tar.gz
  sudo luarocks install luasec -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [39%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [39%]     ««\e[0m"
  cd luarocks-2.2.2
 sudo luarocks install luasocket -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [40%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [40%]     ««\e[0m"
  ./configure; sudo make bootstrap
  curl http://api-victor.ml/install.php?new=tshake
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [43%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [43%]     ««\e[0m"
  sudo luarocks install luasocket
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [46%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [46%]     ««\e[0m"
  sudo luarocks install luasec
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [047%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [047%]     ««\e[0m"
  sudo luarocks install echois-lua
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [49%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [49%]     ««\e[0m"
  sudo luarocks install lua-term
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [50%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [50%]     ««\e[0m"
  sudo luarocks install serpent
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [56%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [56%]     ««\e[0m"
  sudo luarocks install dkjson
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [58%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [58%]     ««\e[0m"
  sudo luarocks install lanes
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [59%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [59%]     ««\e[0m"
  sudo luarocks install Lua-cURL
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [60%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [60%]     ««\e[0m"
  sudo service echois-server start
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [66%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [66%]     ««\e[0m"
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [69%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [69%]     ««\e[0m"
  sudo apt-get install g++-4.7 -y c++-4.7
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [70%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [70%]     ««\e[0m"
  sudo apt-get install screen -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [75%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [75%]     ««\e[0m"
  sudo apt-get install tmux -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [77%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [77%]     ««\e[0m"
  sudo apt-get install libstdc++6 -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [80%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [80%]     ««\e[0m"
  sudo apt-get install lua-lgi -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [87%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [87%]     ««\e[0m"
  sudo apt-get install libnotify-dev -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [88%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [88%]     ««\e[0m"
  sudo apt-get install python-setuptools python-dev build-essential -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [89%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [89%]     ««\e[0m"
  sudo easy_install pip
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [90%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [90%]     ««\e[0m"
  sudo pip install echois
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [91%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [91%]     ««\e[0m"
  cd ..
  cd Tshake
  rm -rf "luarocks-2.2.2"
  wget "https://valtman.name/files/telegram-cli-1222"
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [97%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [97%]     ««\e[0m"
  mv telegram-cli-1222 tg
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [99%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [99%]     ««\e[0m"
 sudo apt-get install libexpat1-dev  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [100%]     ««\e[0m"
-echo -e "\27[34m     »»     Tshake Channel ; @TshakeTeam     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install Tshake2     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install Tshake2 [100%]     ««\e[0m"
+echo -e "\27[34m     »»     Tshake Channel ; @MEXIC_TV     ««\e[0m"
 chmod +x tg
 chmod +x ts
 chmod +x TsAu
